aef311a97d500330802db592dbcb7cdbab6f85e7
kjemison <kurtisjemison@gmail.com>
2022-03-04 20:46:04 -0600

    trailers added

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,16 +1,20 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
 // Data model
+@Parcelize
 data class Movie(val title: String,
                  val movieId: Int,
+                 val voteAverage: Double,
                  private val posterPath: String,
-                 private val backdropPath: String,
-                 val overview: String) {
+                 val overview: String) : Parcelable {
 
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
-    val backdropImageUrl = "https://image.tmdb.org/t/p/w342/$backdropPath"
 
     companion object {
 
@@ -20,13 +24,14 @@ data class Movie(val title: String,
 
             // Parse info from all 20 movies
             for (i in 0 until movieJsonArray.length()) {
-
                 val movieJson = movieJsonArray.getJSONObject(i)
-                movies.add(Movie(movieJson.getString("title"),
-                                 movieJson.getInt("id"),
-                                 movieJson.getString("posterPath"),
-                                 movieJson.getString("backdropPath"),
-                                 movieJson.getString("overview"))
+
+                movies.add(Movie(
+                    movieJson.getString("title"),
+                    movieJson.getInt("id"),
+                    movieJson.getDouble("vote_average"),
+                    movieJson.getString("poster_path"),
+                    movieJson.getString("overview"))
                 )
             }
             return movies
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,19 +1,23 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 private const val TAG = "MovieAdapter"
 
-class MovieAdapter(private val context: Context, private val movies: MutableList<Movie>) :
-    RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+class MovieAdapter(private val context: Context,
+                   private val movies: MutableList<Movie>) :
+    RecyclerView.Adapter<MovieAdapter.ViewHolder>(),
+   View.OnClickListener {
 
     // EXPENSIVE: creates a view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -31,16 +35,36 @@ class MovieAdapter(private val context: Context, private val movies: MutableList
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
 
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
 
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        // Bind data into view
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
         }
+
+        override fun onClick(p0: View?) {
+            val movie = movies[adapterPosition]
+            // Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, DetailActivity::class.java)
+
+            // Need to make data parcelable when we pass it in
+            intent.putExtra("MOVIE_EXTRA", movie)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
